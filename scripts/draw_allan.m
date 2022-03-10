f0d87f613a1c44cea7082c9930e840cf448646c0
fuwei <fuwei@insta360.com>
2022-03-10 18:47:46 +0800

    plot acc as well in matlab script

--- scripts/draw_allan.m
+++ scripts/draw_allan.m
@@ -13,7 +13,7 @@ data_sim_z= dlmread('../data/data_A3_sim_gyr_z.txt');
 data_sim_draw=[data_sim_x data_sim_y data_sim_z] ;
 
 
-figure
+figure('name', 'gyr')
 loglog(dt, data_draw , 'o');
 % loglog(dt, data_sim_draw , '-');
 xlabel('time:sec');                
@@ -22,3 +22,25 @@ ylabel('Sigma:deg/h');
 grid on;                           
 hold on;                           
 loglog(dt, data_sim_draw , '-');
+
+accdt = dlmread('../data/data_A3_acc_t.txt');         
+accdata_x = dlmread('../data/data_A3_acc_x.txt'); 
+accdata_y= dlmread('../data/data_A3_acc_y.txt'); 
+accdata_z = dlmread('../data/data_A3_acc_z.txt'); 
+accdata_draw=[data_x data_y data_z] ;
+
+accdata_sim_x= dlmread('../data/data_A3_sim_acc_x.txt'); 
+accdata_sim_y= dlmread('../data/data_A3_sim_acc_y.txt'); 
+accdata_sim_z= dlmread('../data/data_A3_sim_acc_z.txt'); 
+accdata_sim_draw=[data_sim_x data_sim_y data_sim_z] ;
+
+
+figure('name', 'acc')
+loglog(accdt, accdata_draw , 'o');
+% loglog(dt, data_sim_draw , '-');
+xlabel('time:sec');                
+ylabel('Sigma');             
+% legend('x','y','z');      
+grid on;                           
+hold on;                           
+loglog(accdt, accdata_sim_draw , '-');
